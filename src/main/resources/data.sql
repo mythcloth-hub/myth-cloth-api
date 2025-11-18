9b1d013208c75a0cb26d33c1c7e68421207f5445
Armando Rivas Arzaluz <rivasarmando271084@icloud.com>
2025-11-18 09:12:06 -0600

    Added data initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO distributions (description) VALUES ('Stores');
+INSERT INTO distributions (description) VALUES ('Tamashii Web Shop');
+INSERT INTO distributions (description) VALUES ('Tamashii World Tour');
+INSERT INTO distributions (description) VALUES ('Tamashii Nations');
+INSERT INTO distributions (description) VALUES ('Tamashii Store');
+INSERT INTO distributions (description) VALUES ('Other Limited Edition');
+
+INSERT INTO distributors (name, country, website) VALUES ('BANDAI', 'JP', 'https://tamashii.jp/');
+INSERT INTO distributors (name, country, website) VALUES ('DAM', 'MX', 'https://animexico-online.com/');
+INSERT INTO distributors (name, country, website) VALUES ('DTM', 'MX', null);
+INSERT INTO distributors (name, country, website) VALUES ('DS_DISTRIBUTIONS', 'ES', 'https://www.sddistribuciones.com/');
+INSERT INTO distributors (name, country, website) VALUES ('BLUE_FIN', 'US', 'https://www.bluefincorp.com');
+
