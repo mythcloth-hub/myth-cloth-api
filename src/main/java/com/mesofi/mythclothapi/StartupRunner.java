0f24a858a59d40f2c26d30fb85717ddfaf91a890
Armando Rivas Arzaluz <rivasarmando271084@icloud.com>
2025-12-05 18:07:31 -0600

    Adde one more change

--- src/main/java/com/mesofi/mythclothapi/StartupRunner.java
+++ src/main/java/com/mesofi/mythclothapi/StartupRunner.java
@@ -2,12 +2,14 @@ package com.mesofi.mythclothapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import com.mesofi.mythclothapi.figurines.FigurineService;
 
 @Component
+@Profile("!test")
 public class StartupRunner {
   @Autowired private FigurineService figurineService;
 
