61c63d13028a74014c38c26ab02c8cd08a5a0c0e
Armando Rivas <rivasarmando271084@icloud.com>
2026-03-18 16:47:51 -0600

    Added iniital test

--- src/test/java/com/mesofi/mythclothapi/figurines/FigurineRepositoryTest.java
+++ src/test/java/com/mesofi/mythclothapi/figurines/FigurineRepositoryTest.java
@@ -2,25 +2,25 @@ package com.mesofi.mythclothapi.figurines;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.time.Instant;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.mesofi.mythclothapi.figurines.model.Figurine;
 
-@DataJpaTest // Bootstraps only JPA components + H2
+@SpringBootTest
 @ActiveProfiles("test")
 public class FigurineRepositoryTest {
+
   @Autowired FigurineRepository repository;
 
   @Test
   void save_shouldThrowException_whenCreationDateIsNull() {
+
     // Arrange
-    Figurine figurine = createFigurine(null, null, null, null, null);
+    Figurine figurine = createFigurineWithRequiredFieldsMissing();
 
     // Act + Assert
     assertThatThrownBy(() -> repository.saveAndFlush(figurine))
@@ -29,18 +29,13 @@ public class FigurineRepositoryTest {
             "NULL not allowed for column \"CREATION_DATE\""); // depends on DB dialect
   }
 
-  private Figurine createFigurine(
-      Instant creationDate,
-      Instant updateDate,
-      String normalizedName,
-      String legacyName,
-      String tamashiiUrl) {
+  private Figurine createFigurineWithRequiredFieldsMissing() {
     Figurine figurine = new Figurine();
-    figurine.setCreationDate(creationDate);
-    figurine.setUpdateDate(updateDate);
-    figurine.setNormalizedName(normalizedName);
-    figurine.setLegacyName(legacyName);
-    figurine.setTamashiiUrl(tamashiiUrl);
+    figurine.setCreationDate(null);
+    figurine.setUpdateDate(null);
+    figurine.setNormalizedName(null);
+    figurine.setLegacyName(null);
+    figurine.setTamashiiUrl(null);
     return figurine;
   }
 }
