f804981c0903a8f04f919b9193d13a7912288659
Armando Rivas <rivasarmando271084@icloud.com>
2026-03-18 18:19:23 -0600

    Added controller tests

--- src/test/java/com/mesofi/mythclothapi/figurines/FigurineControllerTest.java
+++ src/test/java/com/mesofi/mythclothapi/figurines/FigurineControllerTest.java
@@ -1,14 +1,37 @@
 package com.mesofi.mythclothapi.figurines;
 
+import static com.mesofi.mythclothapi.utils.ProblemDetailAssertions.containsDetail;
+import static com.mesofi.mythclothapi.utils.ProblemDetailAssertions.hasInstance;
+import static com.mesofi.mythclothapi.utils.ProblemDetailAssertions.hasStatus;
+import static com.mesofi.mythclothapi.utils.ProblemDetailAssertions.hasTimestamp;
+import static com.mesofi.mythclothapi.utils.ProblemDetailAssertions.hasTitle;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(FigurineController.class)
 public class FigurineControllerTest {
 
+  @Autowired private MockMvc mockMvc;
+
   @MockitoBean FigurineService service;
 
   @Test
-  void createFigurine_shouldReturn400_whenBodyIsMissing() throws Exception {}
+  void createFigurine_shouldReturn400_whenBodyIsMissing() throws Exception {
+    mockMvc
+        .perform(post("/figurines"))
+        .andDo(print())
+        .andExpect(status().isBadRequest())
+        .andExpect(hasTitle("Invalid body"))
+        .andExpect(hasStatus(400))
+        .andExpect(containsDetail("Required request body is missing"))
+        .andExpect(hasInstance("/figurines"))
+        .andExpect(hasTimestamp());
+  }
 }
