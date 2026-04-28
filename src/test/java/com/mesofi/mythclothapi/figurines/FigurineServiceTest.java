d45ab8b949d4e16efe0359bb6d98ae1a368560fa
Armando Rivas <rivasarmando271084@icloud.com>
2026-04-28 10:08:16 -0600

    Added more tests

--- src/test/java/com/mesofi/mythclothapi/figurines/FigurineServiceTest.java
+++ src/test/java/com/mesofi/mythclothapi/figurines/FigurineServiceTest.java
@@ -2,6 +2,7 @@ package com.mesofi.mythclothapi.figurines;
 
 import static com.mesofi.mythclothapi.distributors.model.CountryCode.JP;
 import static com.mesofi.mythclothapi.distributors.model.CountryCode.MX;
+import static com.mesofi.mythclothapi.figurinedistributions.model.CurrencyCode.JPY;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
@@ -12,10 +13,15 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.validation.ConstraintViolationException;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +30,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.mesofi.mythclothapi.anniversaries.Anniversary;
 import com.mesofi.mythclothapi.anniversaries.AnniversaryRepository;
+import com.mesofi.mythclothapi.catalogs.dto.CatalogResp;
+import com.mesofi.mythclothapi.catalogs.exceptions.CatalogNotFoundException;
 import com.mesofi.mythclothapi.catalogs.model.Distribution;
 import com.mesofi.mythclothapi.catalogs.model.Group;
 import com.mesofi.mythclothapi.catalogs.model.LineUp;
@@ -36,12 +44,20 @@ import com.mesofi.mythclothapi.config.MapperTestConfig;
 import com.mesofi.mythclothapi.config.MethodValidationTestConfig;
 import com.mesofi.mythclothapi.config.TestCsvConfig;
 import com.mesofi.mythclothapi.distributors.DistributorRepository;
+import com.mesofi.mythclothapi.distributors.dto.DistributorResp;
 import com.mesofi.mythclothapi.distributors.model.Distributor;
 import com.mesofi.mythclothapi.distributors.model.DistributorName;
+import com.mesofi.mythclothapi.figurinedistributions.model.CurrencyCode;
+import com.mesofi.mythclothapi.figurineevents.model.FigurineEventType;
+import com.mesofi.mythclothapi.figurines.dto.DistributorReq;
+import com.mesofi.mythclothapi.figurines.dto.FigurineDistributorResp;
+import com.mesofi.mythclothapi.figurines.dto.FigurineReq;
+import com.mesofi.mythclothapi.figurines.dto.FigurineResp;
 import com.mesofi.mythclothapi.figurines.imports.FigurineCsvSource;
 import com.mesofi.mythclothapi.figurines.mapper.CatalogContext;
 import com.mesofi.mythclothapi.figurines.mapper.FigurineMapper;
 import com.mesofi.mythclothapi.figurines.model.Figurine;
+import com.mesofi.mythclothapi.figurines.model.ReleaseStatus;
 
 @SpringBootTest(
     classes = {
@@ -169,7 +185,423 @@ public class FigurineServiceTest {
     }
   }
 
-  private void mockCatalogRepositories() {
+  @Test
+  @SuppressWarnings("DataFlowIssue")
+  void createFigurine_shouldThrowConstraintViolationException_whenRequestIsNull() {
+    // Act
+    assertThatThrownBy(() -> figurineService.createFigurine(null))
+        .isInstanceOf(ConstraintViolationException.class)
+        .hasMessageContaining("createFigurine.request: must not be null");
+  }
+
+  @Test
+  void createFigurine_shouldThrowConstraintViolationException_whenRequestHasInvalidFields() {
+    // Arrange
+    FigurineReq figurineReq = createFigurineReq(null, null, null, null, null);
+
+    // Act
+    assertThatThrownBy(() -> figurineService.createFigurine(figurineReq))
+        .isInstanceOf(ConstraintViolationException.class)
+        .hasMessageContaining("createFigurine.request.name: must not be blank")
+        .hasMessageContaining("createFigurine.request.lineUpId: must not be null")
+        .hasMessageContaining("createFigurine.request.seriesId: must not be null");
+  }
+
+  @Test
+  void createFigurine_shouldThrowConstraintViolationException_whenRequestHasFieldValueViolations() {
+    // Arrange
+    FigurineReq figurineReq =
+        createFigurineReq(
+            "Pegasus Seiya".repeat(20),
+            "https://tamashiiweb.com/item/15834/".repeat(5),
+            -1L,
+            -1L,
+            null);
+
+    // Act
+    assertThatThrownBy(() -> figurineService.createFigurine(figurineReq))
+        .isInstanceOf(ConstraintViolationException.class)
+        .hasMessageContaining("createFigurine.request.name: Name must not exceed 100 characters")
+        .hasMessageContaining(
+            "createFigurine.request.tamashiiUrl: Tamashii URL must not exceed 50 characters")
+        .hasMessageContaining("createFigurine.request.lineUpId: must be greater than 0")
+        .hasMessageContaining("createFigurine.request.seriesId: must be greater than 0");
+  }
+
+  @Test
+  // @SuppressWarnings("DataFlowIssue")
+  void createFigurine_shouldThrowConstraintViolationException_whenDistributorFieldsAreInvalid() {
+    // Arrange
+    List<DistributorReq> distributors = new ArrayList<>();
+    distributors.add(new DistributorReq(-1L, null, -10.0, null, null, null, null));
+
+    FigurineReq figurineReq =
+        createFigurineReq(
+            "Pegasus Seiya", "https://tamashiiweb.com/item/15834/", 1L, 1L, distributors);
+
+    // Act
+    assertThatThrownBy(() -> figurineService.createFigurine(figurineReq))
+        .isInstanceOf(ConstraintViolationException.class)
+        .hasMessageContaining(
+            "createFigurine.request.distributors[0].price: must be greater than 0")
+        .hasMessageContaining("createFigurine.request.distributors[0].currency: must not be null")
+        .hasMessageContaining(
+            "createFigurine.request.distributors[0].supplierId: must be greater than 0");
+  }
+
+  @Test
+  void createFigurine_shouldThrowCatalogNotFoundException_whenLineUpDoesNotExist() {
+    // Arrange
+    List<DistributorReq> distributors = new ArrayList<>();
+    distributors.add(new DistributorReq(1L, CurrencyCode.JPY, 10.0, null, null, null, null));
+
+    FigurineReq figurineReq =
+        createFigurineReq(
+            "Pegasus Seiya", "https://tamashiiweb.com/item/15834/", 1L, 1L, distributors);
+
+    // Act
+    assertThatThrownBy(() -> figurineService.createFigurine(figurineReq))
+        .isInstanceOf(CatalogNotFoundException.class)
+        .hasMessageContaining("Catalog not found: LineUp not found for id=1");
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void createFigurine_shouldReturnFigurineResp_whenRequestIsBasic(
+      List<DistributorReq> distributorReqList) {
+    // Arrange
+    CatalogContext catalogContext = mockCatalogRepositories();
+
+    FigurineReq figurineReq =
+        createFigurineReq(
+            "Pegasus Seiya", "https://tamashiiweb.com/item/15834/", 1L, 1L, distributorReqList);
+
+    Figurine figurineSaved = mapper.toFigurine(figurineReq, catalogContext);
+    figurineSaved.setId(99L);
+
+    when(figurineRepository.save(any())).thenReturn(figurineSaved);
+
+    // Act
+    FigurineResp figurineResp = figurineService.createFigurine(figurineReq);
+
+    // Assert
+    assertThat(figurineResp)
+        .isNotNull()
+        .extracting(
+            FigurineResp::id,
+            FigurineResp::name,
+            FigurineResp::displayableName,
+            FigurineResp::distributors,
+            FigurineResp::tamashiiUrl,
+            FigurineResp::releaseStatus,
+            FigurineResp::distribution,
+            FigurineResp::lineUp,
+            FigurineResp::series,
+            FigurineResp::group,
+            FigurineResp::anniversary,
+            FigurineResp::isMetalBody,
+            FigurineResp::isOriginalColorEdition,
+            FigurineResp::isRevival,
+            FigurineResp::isPlainCloth,
+            FigurineResp::isBattleDamaged,
+            FigurineResp::isGoldenArmor,
+            FigurineResp::isGold24kEdition,
+            FigurineResp::isMangaVersion,
+            FigurineResp::isMultiPack,
+            FigurineResp::isArticulable,
+            FigurineResp::notes,
+            FigurineResp::officialImageUrls,
+            FigurineResp::unofficialImageUrls,
+            FigurineResp::events,
+            FigurineResp::createdAt,
+            FigurineResp::updatedAt)
+        .containsExactly(
+            99L,
+            "Pegasus Seiya",
+            "FIXME",
+            distributorReqList,
+            "https://tamashiiweb.com/item/15834/",
+            ReleaseStatus.RUMORED,
+            null,
+            new CatalogResp(1, "Myth Cloth EX"),
+            new CatalogResp(1, "Saint Seiya"),
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            List.of(),
+            null,
+            null);
+
+    // Verify
+    verifyCatalogRepositoryInteractions();
+    verify(figurineRepository).save(any());
+  }
+
+  @Test
+  void createFigurine_shouldReturnFigurineResp_whenRequestIsValid() {
+    // Arrange
+    CatalogContext catalogContext = mockCatalogRepositories();
+    List<DistributorReq> distributors = new ArrayList<>();
+    distributors.add(new DistributorReq(1L, CurrencyCode.JPY, 16000.0d, null, null, null, null));
+
+    FigurineReq figurineReq =
+        createFigurineReq(
+            "Pegasus Seiya", "https://tamashiiweb.com/item/15834/", 1L, 1L, distributors);
+
+    Figurine figurineSaved = mapper.toFigurine(figurineReq, catalogContext);
+    figurineSaved.setId(99L);
+
+    when(figurineRepository.save(any())).thenReturn(figurineSaved);
+
+    // Act
+    FigurineResp figurineResp = figurineService.createFigurine(figurineReq);
+
+    // Assert
+    assertThat(figurineResp)
+        .isNotNull()
+        .extracting(
+            FigurineResp::id,
+            FigurineResp::name,
+            FigurineResp::displayableName,
+            FigurineResp::distributors,
+            FigurineResp::tamashiiUrl,
+            FigurineResp::releaseStatus,
+            FigurineResp::distribution,
+            FigurineResp::lineUp,
+            FigurineResp::series,
+            FigurineResp::group,
+            FigurineResp::anniversary,
+            FigurineResp::isMetalBody,
+            FigurineResp::isOriginalColorEdition,
+            FigurineResp::isRevival,
+            FigurineResp::isPlainCloth,
+            FigurineResp::isBattleDamaged,
+            FigurineResp::isGoldenArmor,
+            FigurineResp::isGold24kEdition,
+            FigurineResp::isMangaVersion,
+            FigurineResp::isMultiPack,
+            FigurineResp::isArticulable,
+            FigurineResp::notes,
+            FigurineResp::officialImageUrls,
+            FigurineResp::unofficialImageUrls,
+            FigurineResp::events,
+            FigurineResp::createdAt,
+            FigurineResp::updatedAt)
+        .containsExactly(
+            99L,
+            "Pegasus Seiya",
+            "FIXME",
+            List.of(
+                new FigurineDistributorResp(
+                    new DistributorResp(1, "BANDAI", "Tamashii Nations", "JP", null),
+                    JPY,
+                    16000.0d,
+                    16000.0d,
+                    null,
+                    null,
+                    null,
+                    false)),
+            "https://tamashiiweb.com/item/15834/",
+            ReleaseStatus.RUMORED,
+            null,
+            new CatalogResp(1, "Myth Cloth EX"),
+            new CatalogResp(1, "Saint Seiya"),
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            List.of(),
+            null,
+            null);
+
+    // Verify
+    verifyCatalogRepositoryInteractions();
+    verify(figurineRepository).save(any());
+  }
+
+  @Test
+  void createFigurine_shouldReturnFigurineResp_whenRequestIsValidWithEvents() {
+    // Arrange
+    CatalogContext catalogContext = mockCatalogRepositories();
+    List<DistributorReq> distributors = new ArrayList<>();
+    distributors.add(
+        new DistributorReq(
+            1L,
+            CurrencyCode.JPY,
+            16000.0d,
+            LocalDate.of(2026, 1, 1),
+            LocalDate.of(2026, 2, 2),
+            LocalDate.of(2026, 3, 3),
+            true));
+
+    FigurineReq figurineReq =
+        createFigurineReq(
+            "Pegasus Seiya", "https://tamashiiweb.com/item/15834/", 1L, 1L, distributors);
+
+    Figurine figurineSaved = mapper.toFigurine(figurineReq, catalogContext);
+    figurineSaved.setId(99L);
+
+    when(currencyRegionResolver.resolveCountry(JPY)).thenReturn(JP);
+    when(figurineRepository.save(any())).thenReturn(figurineSaved);
+
+    // Act
+    FigurineResp figurineResp = figurineService.createFigurine(figurineReq);
+
+    // Assert
+    ArgumentCaptor<Figurine> captor = ArgumentCaptor.forClass(Figurine.class);
+
+    verify(figurineRepository).save(captor.capture());
+    Figurine persisted = captor.getValue();
+    assertThat(persisted.getId()).isNull();
+    assertThat(persisted.getEvents().size()).isEqualTo(3);
+    persisted
+        .getEvents()
+        .forEach(
+            event -> {
+              assertThat(event.getFigurine()).isNotNull();
+              assertThat(event.getEventDate())
+                  .isIn(
+                      LocalDate.of(2026, 1, 1), LocalDate.of(2026, 2, 2), LocalDate.of(2026, 3, 3));
+              assertThat(event.isEventDateConfirmed()).isTrue();
+              assertThat(event.getRegion()).isEqualTo(JP);
+              assertThat(event.getType())
+                  .isIn(
+                      FigurineEventType.ANNOUNCEMENT,
+                      FigurineEventType.PREORDER_OPEN,
+                      FigurineEventType.RELEASE);
+              assertThat(event.getDescription())
+                  .isIn(
+                      "First announced as a possible future release.",
+                      "Pre-orders are officially open.",
+                      "The global release date has been officially announced.");
+            });
+
+    assertThat(figurineResp)
+        .isNotNull()
+        .extracting(
+            FigurineResp::id,
+            FigurineResp::name,
+            FigurineResp::displayableName,
+            FigurineResp::distributors,
+            FigurineResp::tamashiiUrl,
+            FigurineResp::releaseStatus,
+            FigurineResp::distribution,
+            FigurineResp::lineUp,
+            FigurineResp::series,
+            FigurineResp::group,
+            FigurineResp::anniversary,
+            FigurineResp::isMetalBody,
+            FigurineResp::isOriginalColorEdition,
+            FigurineResp::isRevival,
+            FigurineResp::isPlainCloth,
+            FigurineResp::isBattleDamaged,
+            FigurineResp::isGoldenArmor,
+            FigurineResp::isGold24kEdition,
+            FigurineResp::isMangaVersion,
+            FigurineResp::isMultiPack,
+            FigurineResp::isArticulable,
+            FigurineResp::notes,
+            FigurineResp::officialImageUrls,
+            FigurineResp::unofficialImageUrls,
+            FigurineResp::events,
+            FigurineResp::createdAt,
+            FigurineResp::updatedAt)
+        .containsExactly(
+            99L,
+            "Pegasus Seiya",
+            "FIXME",
+            List.of(
+                new FigurineDistributorResp(
+                    new DistributorResp(1, "BANDAI", "Tamashii Nations", "JP", null),
+                    JPY,
+                    16000.0d,
+                    17600.0d,
+                    LocalDate.of(2026, 1, 1),
+                    LocalDate.of(2026, 2, 2),
+                    LocalDate.of(2026, 3, 3),
+                    true)),
+            "https://tamashiiweb.com/item/15834/",
+            ReleaseStatus.RELEASED,
+            null,
+            new CatalogResp(1, "Myth Cloth EX"),
+            new CatalogResp(1, "Saint Seiya"),
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            List.of(),
+            null,
+            null);
+
+    // Verify
+    verifyCatalogRepositoryInteractions();
+  }
+
+  private FigurineReq createFigurineReq(
+      String name,
+      String tamashiiUrl,
+      Long lineUpId,
+      Long seriesId,
+      List<DistributorReq> distributors) {
+    return new FigurineReq(
+        name,
+        distributors,
+        tamashiiUrl,
+        null,
+        lineUpId,
+        seriesId,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null);
+  }
+
+  private CatalogContext mockCatalogRepositories() {
     CatalogContext catalogContext =
         new CatalogContext(
             loadDistributors(),
@@ -185,6 +617,8 @@ public class FigurineServiceTest {
     when(seriesRepository.findAll()).thenReturn(catalogContext.series());
     when(groupRepository.findAll()).thenReturn(catalogContext.groups());
     when(anniversaryRepository.findAll()).thenReturn(catalogContext.anniversaries());
+
+    return catalogContext;
   }
 
   private void verifyCatalogRepositoryInteractions() {
