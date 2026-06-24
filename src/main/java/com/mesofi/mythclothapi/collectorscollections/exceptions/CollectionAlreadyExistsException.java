810f403b64d6926e86fb10a2c5f3f67a94a51463
Armando Rivas <rivasarmando271084@icloud.com>
2026-06-23 21:03:31 -0600

    Removed unused variables

--- src/main/java/com/mesofi/mythclothapi/collectorscollections/exceptions/CollectionAlreadyExistsException.java
+++ src/main/java/com/mesofi/mythclothapi/collectorscollections/exceptions/CollectionAlreadyExistsException.java
@@ -6,16 +6,11 @@ import org.springframework.http.HttpStatus;
 
 import com.mesofi.mythclothapi.error.ApiException;
 
-import lombok.Getter;
-
-@Getter
 public class CollectionAlreadyExistsException extends ApiException {
   @Serial private static final long serialVersionUID = -4170723581171178442L;
-  private final String name;
 
   public CollectionAlreadyExistsException(String name) {
     super("Collection with name '%s' already exists".formatted(name));
-    this.name = name;
   }
 
   @Override
--- src/main/java/com/mesofi/mythclothapi/collectorscollections/exceptions/CollectionNotFoundException.java
+++ src/main/java/com/mesofi/mythclothapi/collectorscollections/exceptions/CollectionNotFoundException.java
@@ -6,16 +6,11 @@ import org.springframework.http.HttpStatus;
 
 import com.mesofi.mythclothapi.error.ApiException;
 
-import lombok.Getter;
-
-@Getter
 public class CollectionNotFoundException extends ApiException {
   @Serial private static final long serialVersionUID = -4170723581171178442L;
-  private final Long id;
 
   public CollectionNotFoundException(Long id) {
     super("Collection with id %s was not found".formatted(id));
-    this.id = id;
   }
 
   @Override
