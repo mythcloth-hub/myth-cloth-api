f2f79b9d1da5304dacb6b990cbdebf5ba08cb853
Armando Rivas <rivasarmando271084@icloud.com>
2026-07-01 22:36:53 -0600

    Added new changes and mappings

--- src/main/java/com/mesofi/mythclothapi/collectors/mapper/CollectorMapper.java
+++ src/main/java/com/mesofi/mythclothapi/collectors/mapper/CollectorMapper.java
@@ -7,12 +7,17 @@ import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import com.mesofi.mythclothapi.catalogs.model.LineUp;
 import com.mesofi.mythclothapi.collectorscollections.CollectorCollection;
 import com.mesofi.mythclothapi.collectorscollections.dto.CollectorCollectionFigurineDetailResp;
 import com.mesofi.mythclothapi.collectorscollections.dto.CollectorCollectionFigurineResp;
 import com.mesofi.mythclothapi.collectorscollections.dto.CollectorCollectionResp;
 import com.mesofi.mythclothapi.collectorscollections.model.CollectorCollectionFigurine;
 import com.mesofi.mythclothapi.common.BaseId;
+import com.mesofi.mythclothapi.distributors.dto.DistributorResp;
+import com.mesofi.mythclothapi.distributors.model.Distributor;
+import com.mesofi.mythclothapi.figurinedistributions.model.FigurineDistributor;
+import com.mesofi.mythclothapi.figurines.dto.FigurineDistributorResp;
 import com.mesofi.mythclothapi.figurines.model.Figurine;
 import com.mesofi.mythclothapi.figurines.model.ReleaseStatus;
 
@@ -51,6 +56,39 @@ public interface CollectorMapper {
   }
 
   @Mapping(target = "displayableName", expression = "java(createDisplayableName.apply(figurine))")
+  @Mapping(target = "lineUp", source = "lineup")
+  @Mapping(target = "lineUpUrl", expression = "java(calculateLineUpUrl(figurine.getLineup()))")
   CollectorCollectionFigurineDetailResp toCollectorCollectionFigurineDetailResp(
-      Figurine figurine, @Context Function<Figurine, String> createDisplayableName);
+      Figurine figurine,
+      @Context Function<Figurine, String> createDisplayableName,
+      @Context Function<FigurineDistributor, Double> calculatePriceWithTax);
+
+  @Mapping(target = "description", expression = "java(distributor.getName().getDescription())")
+  @Mapping(target = "countryCode", source = "country")
+  DistributorResp toDistributorResp(Distributor distributor);
+
+  @Mapping(
+      target = "priceWithTax",
+      expression = "java(calculatePriceWithTax.apply(figurineDistributor))")
+  @Mapping(target = "announcedAt", source = "announcementDate")
+  @Mapping(target = "preorderOpensAt", source = "preorderDate")
+  FigurineDistributorResp toFigurineDistributorResp(
+      FigurineDistributor figurineDistributor,
+      @Context Function<Figurine, String> createDisplayableName,
+      @Context Function<FigurineDistributor, Double> calculatePriceWithTax);
+
+  default String calculateLineUpUrl(LineUp lineup) {
+    if (lineup == null) {
+      return null;
+    }
+    if (lineup.getDescription().equals("Myth Cloth EX")) {
+      return "https://imagizer.imageshack.com/img922/1037/VGb1UY.png";
+    } else if (lineup.getDescription().equals("Myth Cloth")) {
+      return "https://imagizer.imageshack.com/img924/6752/iUnW9X.png";
+    } else if (lineup.getDescription().contains("Zero")) {
+      return "https://imagizer.imageshack.com/img924/3571/4Lb8pL.png";
+    }
+    // Todo: Add more lineups and their corresponding URLs as needed
+    return null;
+  }
 }
--- src/main/java/com/mesofi/mythclothapi/collectorscollections/dto/CollectorCollectionFigurineDetailResp.java
+++ src/main/java/com/mesofi/mythclothapi/collectorscollections/dto/CollectorCollectionFigurineDetailResp.java
@@ -1,6 +1,15 @@
 package com.mesofi.mythclothapi.collectorscollections.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.mesofi.mythclothapi.catalogs.dto.CatalogResp;
+import com.mesofi.mythclothapi.figurines.dto.FigurineDistributorResp;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record CollectorCollectionFigurineDetailResp(String displayableName) {}
+public record CollectorCollectionFigurineDetailResp(
+    String displayableName,
+    List<FigurineDistributorResp> distributors,
+    String tamashiiUrl,
+    CatalogResp lineUp,
+    String lineUpUrl) {}
