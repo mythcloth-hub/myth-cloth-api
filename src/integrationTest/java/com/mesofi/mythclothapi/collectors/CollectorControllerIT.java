ac2ecb1e24bfd7a976a5dd53b1b84f83c845c7ee
Armando Rivas <rivasarmando271084@icloud.com>
2026-09-12 00:19:37 -0600

    Added test

--- src/integrationTest/java/com/mesofi/mythclothapi/collectors/CollectorControllerIT.java
+++ src/integrationTest/java/com/mesofi/mythclothapi/collectors/CollectorControllerIT.java
@@ -1,123 +1,131 @@
 package com.mesofi.mythclothapi.collectors;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.mesofi.mythclothapi.collectorproviders.model.ProviderType;
 import com.mesofi.mythclothapi.collectors.dto.CollectorLoginReq;
 import com.mesofi.mythclothapi.collectors.dto.CollectorLoginResp;
-import com.mesofi.mythclothapi.distributors.dto.DistributorReq;
-import com.mesofi.mythclothapi.distributors.dto.DistributorResp;
-import com.mesofi.mythclothapi.distributors.model.CountryCode;
-import com.mesofi.mythclothapi.distributors.model.DistributorName;
-import com.mesofi.mythclothapi.security.permissions.dto.PermissionReq;
-import com.mesofi.mythclothapi.security.permissions.dto.PermissionResp;
-import com.mesofi.mythclothapi.security.rolepermissions.dto.RolePermissionReq;
-import com.mesofi.mythclothapi.security.roles.dto.RoleReq;
-import com.mesofi.mythclothapi.security.roles.dto.RoleResp;
+import com.mesofi.mythclothapi.collectors.dto.CollectorSignupReq;
+import com.mesofi.mythclothapi.collectors.dto.CollectorSignupResp;
+import com.mesofi.mythclothapi.demo.DemoProperties;
 import com.mesofi.mythclothapi.support.ControllerBaseIT;
 
 @AutoConfigureMockMvc
 @Sql(scripts = "/cleanup-collector-it.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class CollectorControllerIT extends ControllerBaseIT {
-    static WireMockServer wireMockServer = new WireMockServer(options().dynamicPort());
 
-    static {
-        wireMockServer.start();
-    }
+    private static final String AUTH_PROVIDER = "/collectors/auth/{provider}";
+    private static final String SIGNUP = "/collectors/signup";
+    private static final WireMockServer FACEBOOK_API = startFacebookApi();
 
-    @AfterAll
-    static void stop() {
-        wireMockServer.stop();
-    }
+    @Autowired
+    private DemoProperties demoProperties;
 
     @DynamicPropertySource
-    static void configure(DynamicPropertyRegistry registry) {
-        registry.add("myth-cloth.facebook.graph-url", () -> "http://localhost:" + wireMockServer.port());
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("myth-cloth.facebook.graph-url", FACEBOOK_API::baseUrl);
+    }
+
+    @BeforeEach
+    void setUp() {
+        FACEBOOK_API.resetAll();
+        FACEBOOK_API.stubFor(get(urlPathEqualTo("/debug_token")).willReturn(okJson("""
+                {
+                    "data": {
+                        "app_id": "0000",
+                        "type": "USER",
+                        "application": "MythCollection-Integration-Test",
+                        "data_access_expires_at": 4102444800,
+                        "expires_at": 4102444800,
+                        "is_valid": true,
+                        "scopes": ["email", "public_profile"],
+                        "user_id": "fb-user-123"
+                    }
+                }
+                """)));
+        FACEBOOK_API.stubFor(get(urlPathEqualTo("/me")).willReturn(okJson("""
+                {
+                    "id": "fb-user-123",
+                    "name": "Facebook Collector",
+                    "email": "demo.integration@saintcollections.com"
+                }
+                """)));
+    }
+
+    @Test
+    @DisplayName("Login with Facebook provider")
+    void loginWithUsingFacebookProvider() {
+        CollectorLoginResp userLoginResp = loginWithProvider(ProviderType.FACEBOOK);
+        System.out.println("User logged in with Facebook: " + userLoginResp);
+    }
+
+    @Test
+    @DisplayName("Login with Google provider")
+    void loginWithUsingGoogleProvider() {
+
     }
 
     // @Test
-    // @DisplayName("Test Facebook login flow and collector authentication")
-    void fullFacebookLoginFlow() {
+    @DisplayName("Sign up using email and password")
+    void signUpUsingEmailAndPassword() {
+        CollectorSignupReq request = new CollectorSignupReq("Seiya Collector", "collector.signup@mythcloth.dev",
+                "MythCloth1!");
 
-        final String fbFakeAccessToken = "fbFakeAccessToken";
+        ResponseEntity<CollectorSignupResp> response = rest.post().uri(SIGNUP).body(request).retrieve()
+                .toEntity(CollectorSignupResp.class);
 
-        // Mock Facebook token validation
-        wireMockServer.stubFor(get(urlPathEqualTo("/debug_token")).withQueryParam("access_token", equalTo("0000|1111"))
-                .withQueryParam("input_token", equalTo(fbFakeAccessToken)).willReturn(okJson("""
-                            {
-                              "data": {
-                                "app_id": "0000",
-                                "type": "USER",
-                                "application": "MyApp",
-                                "data_access_expires_at": 1789055926,
-                                "expires_at": 1789055926,
-                                "is_valid": true,
-                                "scopes": [
-                                  "email", "public_profile"
-                                ],
-                                "user_id": "1234567890"
-                              }
-                            }
-                        """)));
-
-        // Mock Facebook user information
-        wireMockServer.stubFor(get(urlPathEqualTo("/me")).withQueryParam("access_token", equalTo(fbFakeAccessToken))
-                .willReturn(okJson("""
-                        {
-                          "id": "1234567890",
-                          "name": "Facebook Testing User",
-                          "email": "test@facebook.com"
-                        }
-                        """)));
-
-        // Prepare the setup for this scenario.
-
-        PermissionResp distributorWritePermission = rest.post().uri("/permissions")
-                .body(new PermissionReq("distributors:create")).retrieve().body(PermissionResp.class);
-
-        PermissionResp distributorReadPermission = rest.post().uri("/permissions")
-                .body(new PermissionReq("distributors:read")).retrieve().body(PermissionResp.class);
-
-        RoleResp adminRole = rest.post().uri("/roles").body(new RoleReq("Admin")).retrieve().body(RoleResp.class);
-
-        assert adminRole != null;
-        assert distributorWritePermission != null;
-        assert distributorReadPermission != null;
-
-        assignPermissionToRole(adminRole.id(), distributorWritePermission.id());
-        assignPermissionToRole(adminRole.id(), distributorReadPermission.id());
-
-        // The user logs in using Facebook and receives a JWT token from our system.
-        // This token should
-        // have the permissions assigned to the Admin role.
-
-        CollectorLoginResp collectorLoginResp = rest.post().uri("/collectors/auth/{provider}", "facebook")
-                .body(new CollectorLoginReq(null, fbFakeAccessToken, null, null)).retrieve()
-                .body(CollectorLoginResp.class);
-
-        assert collectorLoginResp != null;
-
-        // Call the protected resource using the collector's JWT token and verify access
-        // is granted due
-        // to the Admin role permissions.
-        rest.post().uri("/distributors")
-                .headers(httpHeaders -> httpHeaders.setBearerAuth(collectorLoginResp.accessToken()))
-                .body(new DistributorReq(DistributorName.BANDAI, CountryCode.JP, "www.google.com")).retrieve()
-                .toEntity(DistributorResp.class);
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().collectorId()).isPositive();
+        assertThat(response.getBody().fullName()).isEqualTo(request.fullName());
+        assertThat(response.getBody().email()).isEqualTo(request.email());
     }
 
-    private void assignPermissionToRole(long roleId, long permissionId) {
-        rest.post().uri("/roles/{roleId}/permissions", roleId).body(new RolePermissionReq(permissionId)).retrieve()
-                .toBodilessEntity();
+    private CollectorLoginResp loginWithProvider(ProviderType providerType) {
+        CollectorLoginReq request = new CollectorLoginReq("dummy-id-token", "dummy-access-token",
+                demoProperties.email(), null);
+
+        ResponseEntity<CollectorLoginResp> response = rest.post().uri(AUTH_PROVIDER, providerType).body(request)
+                .retrieve().toEntity(CollectorLoginResp.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().collectorId()).isPositive();
+        assertThat(response.getBody().displayName()).isEqualTo("Facebook Collector");
+        assertThat(response.getBody().email()).isEqualTo(demoProperties.email());
+        assertThat(response.getBody().role()).isEqualTo("Collector");
+        assertThat(response.getBody().accessToken()).isNotBlank();
+        assertThat(response.getBody().tokenType()).isEqualTo("Bearer");
+        assertThat(response.getBody().expiresInSeconds()).isPositive();
+
+        return response.getBody();
+    }
+
+    @AfterAll
+    static void stopWireMock() {
+        FACEBOOK_API.stop();
+    }
+
+    private static WireMockServer startFacebookApi() {
+        WireMockServer wireMockServer = new WireMockServer(wireMockConfig().dynamicPort());
+        wireMockServer.start();
+        return wireMockServer;
     }
 }
