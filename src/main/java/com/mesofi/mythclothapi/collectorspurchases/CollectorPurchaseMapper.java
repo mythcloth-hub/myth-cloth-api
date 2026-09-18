fd07c721bf6d8de2f769f225f963fdbbb2560c18
Armando Rivas <rivasarmando271084@icloud.com>
2026-09-17 21:21:54 -0600

    Updating mapper

--- src/main/java/com/mesofi/mythclothapi/collectorspurchases/CollectorPurchaseMapper.java
+++ src/main/java/com/mesofi/mythclothapi/collectorspurchases/CollectorPurchaseMapper.java
@@ -8,21 +8,84 @@ import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import com.mesofi.mythclothapi.collectorspurchases.dto.CollectorPurchaseFigurineReq;
 import com.mesofi.mythclothapi.collectorspurchases.dto.CollectorPurchaseReq;
 import com.mesofi.mythclothapi.collectorspurchases.dto.CollectorPurchaseResp;
+import com.mesofi.mythclothapi.collectorspurchases.model.CollectorPurchaseFigurine;
 import com.mesofi.mythclothapi.common.CurrencyCode;
 
+/**
+ * Mapper responsible for converting between {@link CollectorPurchaseReq} and
+ * {@link CollectorPurchaseFigurineReq} DTOs and their corresponding entity
+ * classes, as well as mapping from {@link CollectorPurchase} to
+ * {@link CollectorPurchaseResp}.
+ *
+ * <p>
+ * This mapper uses MapStruct to generate the implementation at compile time and
+ * is registered as a Spring component.
+ */
 @Mapper(componentModel = "spring")
 public interface CollectorPurchaseMapper {
 
+    /**
+     * Converts a {@link CollectorPurchaseReq} into a {@link CollectorPurchase}.
+     *
+     * <p>
+     * The {@code id}, {@code creationDate}, and {@code updateDate} fields are
+     * ignored because a new entity is being created. The {@code collector} field is
+     * also ignored since it will be set in the service layer. The {@code orderDate}
+     * is mapped from the request's {@code purchaseDate}, while the
+     * {@code shippedDate} and {@code deliveredDate} are ignored as they will be set
+     * later in the purchase lifecycle.
+     *
+     * @param request
+     *            the request DTO containing the purchase data
+     * @return a new {@link CollectorPurchase} populated from the request
+     */
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "creationDate", ignore = true)
     @Mapping(target = "updateDate", ignore = true)
+    @Mapping(target = "collector", ignore = true) // The collector will be set in the service layer
     @Mapping(target = "orderDate", source = "purchaseDate")
     @Mapping(target = "shippedDate", ignore = true)
     @Mapping(target = "deliveredDate", ignore = true)
     CollectorPurchase toCollectorPurchase(CollectorPurchaseReq request);
 
+    /**
+     * Converts a {@link CollectorPurchaseFigurineReq} into a
+     * {@link CollectorPurchaseFigurine}.
+     *
+     * <p>
+     * The {@code id}, {@code creationDate}, and {@code updateDate} fields are
+     * ignored because a new entity is being created. The {@code purchase} and
+     * {@code collectionFigurine} fields are also ignored since they will be set in
+     * the service layer.
+     *
+     * @param request
+     *            the request DTO containing the figurine data
+     * @return a new {@link CollectorPurchaseFigurine} populated from the request
+     */
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationDate", ignore = true)
+    @Mapping(target = "updateDate", ignore = true)
+    @Mapping(target = "purchase", ignore = true)
+    @Mapping(target = "collectionFigurine", ignore = true)
+    CollectorPurchaseFigurine toCollectorPurchaseFigurine(CollectorPurchaseFigurineReq request);
+
+    /**
+     * Converts a {@link CollectorPurchase} into a {@link CollectorPurchaseResp}.
+     *
+     * <p>
+     * The {@code purchaseId} is mapped from the entity's {@code id}, and the
+     * {@code totalAmount} is calculated using the provided function. Other fields
+     * are mapped directly from the entity.
+     *
+     * @param purchase
+     *            the entity to be converted
+     * @param calculateTotalAmount
+     *            a function to calculate the total amount of the purchase
+     * @return a new {@link CollectorPurchaseResp} populated from the entity
+     */
     @Mapping(target = "purchaseId", source = "id")
     @Mapping(target = "totalAmount", expression = "java(calculateTotalAmount.apply(purchase))")
     CollectorPurchaseResp toCollectorPurchaseResp(CollectorPurchase purchase,
