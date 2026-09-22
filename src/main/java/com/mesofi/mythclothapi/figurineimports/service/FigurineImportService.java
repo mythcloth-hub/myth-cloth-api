876749b07ee51d019d5844b7bba76615f55d0cd4
brocoly-dev <rivasarmando271084@icloud.com>
2026-09-21 18:23:03 -0600

    Added caching to the recommended figurines (#67)

--- src/main/java/com/mesofi/mythclothapi/figurineimports/service/FigurineImportService.java
+++ src/main/java/com/mesofi/mythclothapi/figurineimports/service/FigurineImportService.java
@@ -4,6 +4,7 @@ import static com.mesofi.mythclothapi.collectorscollections.CollectorCollectionF
 import static com.mesofi.mythclothapi.collectorscollections.CollectorCollectionFigurineService.COLLECTOR_SUMMARY_CACHE;
 import static com.mesofi.mythclothapi.figurines.FigurineService.FIGURINE_CACHE;
 import static com.mesofi.mythclothapi.figurines.FigurineService.FIGURINE_SUMMARY_CACHE;
+import static com.mesofi.mythclothapi.figurines.FigurineService.RECOMMENDED_FIGURINE_CACHE;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -100,7 +101,7 @@ public class FigurineImportService {
      *             the import
      */
     @Transactional
-    @CacheEvict(value = {FIGURINE_CACHE, FIGURINE_SUMMARY_CACHE, COLLECTOR_SUMMARY_CACHE,
+    @CacheEvict(value = {FIGURINE_CACHE, FIGURINE_SUMMARY_CACHE, RECOMMENDED_FIGURINE_CACHE, COLLECTOR_SUMMARY_CACHE,
             COLLECTOR_FIGURINE_CACHE}, allEntries = true)
     public void importAllFigurinesFromPublicDrive() {
         log.info("Importing all figurines from public drive...");
